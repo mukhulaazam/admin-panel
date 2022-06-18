dc0a0172ddce10cb2260225f9b7039360c30bf6b
mukhulaazam <mukhulaazam@gmail.com>
2022-06-18 07:45:08 +0600

    Design appeared

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import TopHeader from './components/TopHeader.vue';
+import TopNav from './components/NavBar.vue';
 
 </script>
 <template>
   <header>
     <TopHeader></TopHeader>
   </header>
+  <nav>
+    <TopNav></TopNav>
+  </nav>
 
 </template>
 <style>
@@ -14,5 +18,14 @@ import TopHeader from './components/TopHeader.vue';
 @import './assets/css/owl.carousel.css';
 @import './assets/css/owl.theme.css';
 @import './assets/css/owl.transitions.css';
+@import './assets/css/meanmenu/meanmenu.min.css';
+@import './assets/css/animate.css';
+@import './assets/css/normalize.css';
+@import './assets/css/scrollbar/jquery.mCustomScrollbar.min.css';
+@import './assets/css/jvectormap/jquery-jvectormap-2.0.3.css';
+@import './assets/css/notika-custom-icon.css';
+@import './assets/css/wave/waves.min.css';
 @import './assets/css/main.css';
+@import './assets/css/style.css';
+@import './assets/css/responsive.css';
 </style>
